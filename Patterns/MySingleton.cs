d724f53a75aa1f1cdf131d0f2fe4c6ddb86fd2b0
leonardo-fm <noblegas38@gmail.com>
2024-06-17 22:04:40 +0200

    Added singleton

---
+++ Patterns/MySingleton.cs
@@ -0,0 +1,16 @@
+﻿namespace Singleton;
+
+public class MySingleton
+{
+    private static MySingleton? instance;
+
+    public static MySingleton GetInstance()
+    {
+        if (instance == null)
+            instance = new MySingleton();
+
+        return instance;
+    }
+
+    public string GetGreetings() => "Hi!";
+}
